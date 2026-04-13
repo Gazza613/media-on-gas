e081d60e17b4665a952106f8cb09321fbc2d6164
GAS Marketing <gary@gasmarketing.co.za>
2026-04-13 20:38:58 +0000

    Fix community page matching - no bash exclamation issues

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -720,17 +720,25 @@ export default function MediaOnGas(){
               var matchedPages2=[];var matchedIds2={};
               for(var p=0;p<pages.length;p++){
                 var pg=pages[p];
+                var pgMatched=false;
                 for(var s=0;s<sel.length;s++){
                   var sc3=autoMatchPage(sel[s].campaignName,pg.name);
+                  if(sc3>0){pgMatched=true;break;}
+                }
+                if(pgMatched&&matchedIds2[pg.id]!==true){matchedPages2.push(pg);matchedIds2[pg.id]=true;}
+                if(matchedIds2[pg.id]!==true){
+                  for(var s2=0;s2<sel.length;s2++){
+                    var cn2=(sel[s2].campaignName||"").toLowerCase();
+                    for(var oi2=0;oi2<pageOverrides.length;oi2++){if(cn2.indexOf(pageOverrides[oi2].campaign)>=0&&(pg.name||"").toLowerCase().indexOf(pageOverrides[oi2].page)>=0){matchedPages2.push(pg);matchedIds2[pg.id]=true;break;}}
+                    if(matchedIds2[pg.id]===true)break;
+                  }
                 }
               }
-              if(matchedPages2.length===1){fbPage=matchedPages2[0];}
               var fbTotal=0;var igTotal=0;
-              if(matchedPages2.length>1){
+              if(matchedPages2.length>0){
                 matchedPages2.forEach(function(mp){fbTotal+=mp.fan_count||0;if(mp.instagram_business_account){igTotal+=mp.instagram_business_account.followers_count||0;}});
-              }else if(fbPage){
-                fbTotal=fbPage.fan_count||0;
-                if(fbPage.instagram_business_account){igAccount=fbPage.instagram_business_account;igTotal=igAccount.followers_count||0;}
+                fbPage=matchedPages2[0];
+                if(fbPage.instagram_business_account){igAccount=fbPage.instagram_business_account;}
               }
               var ttTotal=(function(){var selNames2=sel.map(function(x){return x.campaignName;}).join(" ");return getTtTotal(selNames2,ttEarned);})();var grandTotal=fbTotal+igTotal+ttTotal;
               return <div>
