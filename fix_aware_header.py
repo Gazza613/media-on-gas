b347b40d23b02d9ed7acf15357a66e32101c6deb
GAS Marketing <gary@gasmarketing.co.za>
2026-04-13 20:50:41 +0000

    Clean up temp fix files

--- fix_aware_header.py
+++
@@ -1,9 +0,0 @@
-with open("/workspaces/media-on-gas/dashboard/src/App.jsx", "r") as f:
-    c = f.read()
-
-# The awareness table uses #002a40 borders instead of #003a55
-c = c.replace('background:"#004F71",border:"1px solid #002a40"', 'background:"#F96203",border:"1px solid #E85500"')
-
-with open("/workspaces/media-on-gas/dashboard/src/App.jsx", "w") as f:
-    f.write(c)
-print("Done - awareness header fixed")
--- fix_url.py
+++
@@ -1,8 +0,0 @@
-with open("/workspaces/media-on-gas/dashboard/src/App.jsx", "r") as f:
-    c = f.read()
-
-c = c.replace("LOOKER_URL_PLACEHOLDER", "https://lookerstudio.google.com/u/0/embed/reporting/823fd5fa-b39d-4dc3-b623-549197d0341f/page/p_2upnicpx0d")
-
-with open("/workspaces/media-on-gas/dashboard/src/App.jsx", "w") as f:
-    f.write(c)
-print("Done - Looker URL added")
